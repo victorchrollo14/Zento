9cd94404ed9cc351642b5cc9748fa6846948afdb
Victor <chrollolucilfer1402@gmail.com>
2023-07-11 19:25:26 +0530

    added some parallax effect

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,18 +2,29 @@ import React from "react";
 import { featuresData } from "./data";
 import { Form } from "./form";
 import { Section } from "./animation";
+import { delay, motion } from "framer-motion";
 
 export const Features = () => {
   return (
     <section className="features-section my-16 mx-4  md:my-12 lg:mx-12">
       <header className="mt-32">
-        <h1 className="uppercase font-mont font-bold text-[28px] text-center cap-header ">
+        <motion.h1
+          initial={{ translateY: "100px", opacity: 0 }}
+          whileInView={{ translateY: 0, opacity: 1 }}
+          transition={{ duration: 0.3, ease: "easeIn" }}
+          className="uppercase font-mont font-bold text-[28px] text-center cap-header "
+        >
           our features
-        </h1>
-        <h3 className="font-mont text-center text-base ">
+        </motion.h1>
+        <motion.h3
+          initial={{ translateY: "100px", opacity: 0 }}
+          whileInView={{ translateY: 0, opacity: 1 }}
+          transition={{ duration: 0.5, ease: "easeIn", delay: 0.3 }}
+          className="font-mont text-center text-base "
+        >
           Unlock a World of Possibilities with <br />
           our Powerful Features
-        </h3>
+        </motion.h3>
       </header>
       <ul className="features ">
         <ListItems />
@@ -34,12 +45,18 @@ export const Features = () => {
 
 const ListItems = () => {
   let id = 0;
+  let translateText = "-30px";
+  let translateImg = "60px";
+
+
   let items = featuresData.map(
     ({ idName, title, subtitle, content, bgColor, textColor, image }) => {
       id++;
       let classList = `list-inside-wrapper py-[10%] md:py-10 sm:w-4/5 flex flex-col gap-7 items-center md:flex-row  justify-between  lg:mx-16 xl:max-w-[1000px]`;
       if (id % 2 == 0) {
         classList = `list-inside-wrapper py-[10%] md:py-10 sm:w-4/5 flex flex-col gap-7 items-center md:flex-row-reverse  justify-between  lg:mx-16 xl:max-w-[1000px]`;
+        translateText = "30px";
+        translateImg = "-60px";
       }
 
       return (
@@ -55,17 +72,34 @@ const ListItems = () => {
                 "lg:-translate-y-10 xl:-translate-y-16"
               }`}
             >
-              <h1 className="uppercase font-mont font-bold text-[28px] cap-header mb-3">
+              <motion.h1
+                initial={{ translateX: translateText, opacity: 0 }}
+                whileInView={{ translateX: 0, opacity: 1 }}
+                transition={{ duration: 0.4, ease: "easeIn" }}
+                viewport={{ amount: 0.5 }}
+                className="uppercase font-mont font-bold text-[28px] cap-header mb-3"
+              >
                 {title}
-              </h1>
-              <h3 className="font-mont font-semibold text-xl mb-2">
-                {subtitle}
-              </h3>
-              <p className="font-mont text-base">{content}</p>
+              </motion.h1>
+              <motion.div
+                initial={{ translateX: translateText, opacity: 0 }}
+                whileInView={{ translateX: 0, opacity: 1 }}
+                transition={{ duration: 0.4, ease: "easeIn", delay: 0.2 }}
+              >
+                <h3 className="font-mont font-semibold text-xl mb-2">
+                  {subtitle}
+                </h3>
+                <p className="font-mont text-base">{content}</p>
+              </motion.div>
             </div>
-            <div className="image w-[90%]  max-w-lg md:w-1/2 xl:w-[700px]">
+            <motion.div
+              initial={{ translateX: translateImg, opacity: 0 }}
+              whileInView={{ translateX: 0, opacity: 1 }}
+              transition={{ duration: 0.4, ease: "easeIn", delay: 0.2 }}
+              className="image w-[90%]  max-w-lg md:w-1/2 xl:w-[700px]"
+            >
               {image}
-            </div>
+            </motion.div>
           </div>
         </li>
       );
