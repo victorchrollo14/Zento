9474a21128cb5e74970a9dd7f7e7571d8e208c8f
Victor <chrollolucilfer1402@gmail.com>
2023-07-13 08:43:08 +0530

    modified svg images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,24 +4,28 @@ import Typewriter from "typewriter-effect";
 export const HeroContent = () => {
   return (
     <div className="hero-content  mt-8 flex flex-col md:justify-center md:items-center lg:items-start">
-      <Typewriter
-        onInit={(typewriter) => {
-          typewriter
-            .typeString("Banking Reimagined... <br />")
-            .typeString(
-              'For the <span class="text-light-green">Gen-Z.</span> <br />'
-            )
-            .typeString('By the <span class="text-light-green">Gen-Z.</span>')
-            .start();
-        }}
-        options={{
-          wrapperClassName:
-            "font-black font-mont text-black text-6xl 2xl:text-6xl text-left hero-header md:text-center lg:leading-none lg:text-left",
-          cursorClassName: "custom-cursor", // Add CSS class for customizing the cursor if needed
-          delay: 10,
-          deleteSpeed: 20,
-        }}
-      />
+      <h1 className="md:text-center lg:text-left">
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter
+              .typeString("Banking Reimagined... <br />")
+              .typeString(
+                'For the <span class="text-light-green md:text-center">Gen-Z.</span> <br />'
+              )
+              .typeString(
+                'By the <span class="text-light-green md:text-center">Gen-Z.</span>'
+              )
+              .start()
+          }}
+          options={{
+            wrapperClassName:
+              "font-black font-mont text-black text-6xl 2xl:text-6xl text-left hero-header md:text-center lg:leading-none lg:text-left",
+            cursorClassName: "custom-cursor", // Add CSS class for customizing the cursor if needed
+            delay: 15,
+            deleteSpeed: 20,
+          }}
+        />
+      </h1>
 
       <h3 className="font-normal text-left  font-mont mt-4 2xl:text-2xl 2xl:max-w-lg max-w-sm md:text-center lg:text-left">
         <span className="text-drkYellow font-bold ">
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -32,7 +32,7 @@ const Home = () => {
         <section className="hero-section mix-bg  lg:min-h-screen ">
           <div className="wrapper p-4 flex justify-center items-center flex-col md:w-screen ">
             <div className="lg:w-[92%]  md:flex md:flex-col lg:flex-row md:justify-between  mt-16">
-              <div className="hero-content md:mt-16  md:w-full lg:w-1/2 2xl:w-[60vw] md:flex flex-col md:items-center lg:items-start">
+              <div className="hero-content md:mt-16  md:w-full lg:w-1/2  md:flex flex-col md:items-center lg:items-start">
                 <HeroContent />
                 <form
                   action=""
@@ -46,24 +46,24 @@ const Home = () => {
                   <input
                     type="email"
                     placeholder="enter your email"
-                    className="placeholder:text-center text-center text-black md:h-12 font-mont w-full rounded-full outline-lYellow 2xl:h-16 2xl:w-[500px] 2xl:text-2xl"
+                    className="placeholder:text-center text-center text-black md:h-12 font-mont w-full rounded-full outline-lYellow 2xl:h-14 2xl:w-[500px] 2xl:text-2xl"
                   />{" "}
                     <button
                       type="submit"
-                      className="text-center font-semibold font-mont bg-lYellow w-full h-10 md:h-12 2xl:h-16 2xl:w-[500px] 2xl:text-2xl rounded-full mt-2 hover:bg-drkYellow"
+                      className="text-center font-semibold font-mont bg-lYellow w-full h-10 md:h-12 2xl:h-14 2xl:w-[500px] 2xl:text-2xl rounded-full mt-2 hover:bg-drkYellow"
                     >
                       Subscribe
                     </button>
                 </form>
               </div>
-              <div className=" md:w-screen lg:w-1/2 flex md:justify-center md:items-center 2xl:w-[580px]">
+              <div className=" md:w-screen lg:w-1/2 flex md:justify-center md:items-center ">
                 <div className="hero-image w-full flex justify-center mt-10 md:hidden ">
                   <MobHeroImage />
                 </div>
                 <RightSlideAnimation>
                   <div className="hero-tab-desktop hidden md:block md:w-[80vw] lg:w-full mt-14">
                     <img
-                      src={tabHeroImage}
+                      src={desktopHero}
                       alt=""
                       className="w-[90%] md:w-full  h-auto "
                     />
